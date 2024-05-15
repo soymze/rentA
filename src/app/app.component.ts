5b04bd159d6225392a9071817abbf7972a75dc48
Emre <soylmze@gmail.com>
2024-05-15 09:19:15 +0300

    First Draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { ArticleComponent } from "./components/article/article.component";
+import { FooterComponent } from "./components/footer/footer.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, ArticleComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'rentA';
