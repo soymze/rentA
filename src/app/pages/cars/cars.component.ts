484ba7d90ed6b7141853408ccdd18bad266257a2
Emre <soylmze@gmail.com>
2024-05-15 22:30:00 +0300

    draft of cars page

--- src/app/pages/cars/cars.component.ts
+++ src/app/pages/cars/cars.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
-
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 }
