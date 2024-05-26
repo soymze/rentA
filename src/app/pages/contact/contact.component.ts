fb5c9b28457d4b054f65531513fd876f7aa88684
Emre <soylmze@gmail.com>
2024-05-26 20:32:19 +0300

    validation for signup and contact services

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,27 +1,46 @@
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  isFormSubmited: boolean = false;
 
-  constructor(private http: HttpClient){
+  contactObj: any = {
+    senderMail: '',
+    senderName: '',
+    subject: '',
+    message: '',
+  };
 
-  }
+  constructor(private http: HttpClient) {}
 
-  onMessageCreate(data: {senderName: string, senderMail: string, subject: string, message: string}) {
+  onMessageCreate(data: {
+    senderName: string;
+    senderMail: string;
+    subject: string;
+    message: string;
+  }) {
+    this.isFormSubmited = true;
     const header = new HttpHeaders({
-      contentType: 'application/json'
-    })
-    console.log(data);
-    this.http.post('http://localhost:8080/api/messages', data, {headers: header}).subscribe((res) => {
-      console.log(res);
+      contentType: 'application/json',
     });
+    console.log(data);
+    this.http
+      .post('http://localhost:8080/api/messages', data, { headers: header })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,19 +1,29 @@
-import { HttpClientModule, HttpHeaders , HttpClient} from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpHeaders,
+  HttpClient,
+} from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  isUserCreated: boolean = false;
 
-  constructor(private http: HttpClient){
+  userObj: any = {
+    userName: '',
+    mail: '',
+    password: '',
+  };
 
-  }
+  constructor(private http: HttpClient) {}
 
   onSignUpClick() {
     const container = document.getElementById('container');
@@ -29,13 +39,16 @@ export class SignupComponent {
     }
   }
 
-  onUserCreate(user: {userName: string, mail: string, password: string}) {
+  onUserCreate(user: { userName: string; mail: string; password: string }) {
+    this.isUserCreated = true;
     const header = new HttpHeaders({
-      contentType: 'application/json'
-    })
-    console.log(user);
-    this.http.post('http://localhost:8080/api/users', user, {headers: header}).subscribe((res) => {
-      console.log(res);
+      contentType: 'application/json',
     });
+    console.log(user);
+    this.http
+      .post('http://localhost:8080/api/users', user, { headers: header })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
